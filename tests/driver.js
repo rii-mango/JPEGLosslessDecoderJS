f6edda5fd9b21c20569c1355b1e6af0d15c0b9c0
Michael Martinez <martinezmj@uthscsa.edu>
2015-06-17 18:18:59 -0700

    added test

---
+++ tests/driver.js
@@ -0,0 +1,31 @@
+
+/*jslint browser: true, node: true */
+/*global require, module */
+
+"use strict";
+
+var jpeg = {};
+jpeg.lossless = {};
+jpeg.lossless.Decoder = require('../src/decoder.js');
+
+var fs = require('fs');
+
+
+function toArrayBuffer(buffer) {
+    var ab, view, i;
+
+    ab = new ArrayBuffer(buffer.length);
+    view = new Uint8Array(ab);
+    for (i = 0; i < buffer.length; i += 1) {
+        view[i] = buffer[i];
+    }
+    return ab;
+}
+
+var buf = fs.readFileSync('./data/data.lossless');
+var data = toArrayBuffer(buf);
+var decoder = new jpeg.lossless.Decoder(data, 2);
+var output = decoder.decode();
+console.log("compressed size = " + data.byteLength);
+console.log("frame: dimX="+decoder.frame.dimX + " dimY=" + decoder.frame.dimY + " components=" + decoder.frame.numComp);
+console.log("decompressed size = " + output.byteLength);
